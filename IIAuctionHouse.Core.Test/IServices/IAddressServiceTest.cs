acd139fece605294baf7475f535475e7418d0ca0
DariusTarvydas00 <79032020+DariusTarvydas00@users.noreply.github.com>
2021-11-29 10:33:08 +0100

    Checking if IAddressService is available

--- IIAuctionHouse.Core.Test/IServices/IAddressServiceTest.cs
+++ IIAuctionHouse.Core.Test/IServices/IAddressServiceTest.cs
@@ -1,7 +1,17 @@
-﻿namespace IIAuctionHouse.Core.Test.IServices
+﻿using IIAuctionHouse.Core.IServices;
+using Moq;
+using Xunit;
+
+namespace IIAuctionHouse.Core.Test.IServices
 {
     public class IAddressServiceTest
     {
-        
+        // Checking if IAddressService is available
+        [Fact]
+        public void IAddressService_IsAvailable()
+        {
+            var service = new Mock<IAddressService>();
+            Assert.NotNull(service);
+        }
     }
 }
