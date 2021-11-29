69793d1e9cfdb86ceeb846eeb8a5671442d021f9
DariusTarvydas00 <79032020+DariusTarvydas00@users.noreply.github.com>
2021-11-29 09:35:45 +0100

    Added reference from Core.Test to Core; Checking if Address class can be initialized

--- IIAuctionHouse.Core.Test/Models/AddressTest.cs
+++ IIAuctionHouse.Core.Test/Models/AddressTest.cs
@@ -1,7 +1,22 @@
-﻿namespace IIAuctionHouse.Core.Test.Models
+﻿using IIAuctionHouse.Core.Models;
+using Xunit;
+
+namespace IIAuctionHouse.Core.Test.Models
 {
     public class AddressTest
     {
-        
+        private Address _address;
+
+        public AddressTest()
+        {
+            _address = new Address();
+        }
+
+        // Checking if Address class can be initialized
+        [Fact]
+        public void Address_CanBeInitialized()
+        {
+            Assert.NotNull(_address);
+        }
     }
 }
