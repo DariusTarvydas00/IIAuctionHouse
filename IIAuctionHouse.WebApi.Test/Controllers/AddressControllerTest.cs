b0e7f09a4d225a4237cce214437d68de7b041829
DariusTarvydas00 <79032020+DariusTarvydas00@users.noreply.github.com>
2021-11-30 00:34:03 +0100

    Checks if AddressController has AddressService and is controller base type Added required files

---
+++ IIAuctionHouse.WebApi.Test/Controllers/AddressControllerTest.cs
@@ -0,0 +1,23 @@
+﻿using IIAuctionHouse.Controllers;
+using IIAuctionHouse.Core.IServices;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace IIAuctionHouse.WebApi.Test.Controllers
+{
+    public class AddressControllerTest
+    {
+        #region Controller Initialization
+        // Checks if AddressController has AddressService and is controller base type
+        [Fact]
+        public void AddressController_HasAddressService_IsTypeOfController()
+        {
+            var service = new Mock<IAddressService>();
+            var controller = new AddressController(service.Object);
+            Assert.IsAssignableFrom<ControllerBase>(controller);
+        }
+
+        #endregion
+    }
+}
---
+++ IIAuctionHouse.WebApi/Controllers/AddressController.cs
@@ -0,0 +1,15 @@
+﻿using IIAuctionHouse.Core.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IIAuctionHouse.Controllers
+{
+    public class AddressController: ControllerBase
+    {
+        private readonly IAddressService _addressService;
+
+        public AddressController(IAddressService addressService)
+        {
+            _addressService = addressService;
+        }
+    }
+}
