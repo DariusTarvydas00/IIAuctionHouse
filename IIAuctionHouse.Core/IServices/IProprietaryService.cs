4872472f458eb4bb244537470c4dfebeabb27ab4
DariusTarvydas00 <79032020+DariusTarvydas00@users.noreply.github.com>
2021-11-30 20:25:55 +0100

    Proprietary service test

---
+++ IIAuctionHouse.Core/IServices/IProprietaryService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using IIAuctionHouse.Core.Models;
+
+namespace IIAuctionHouse.Core.IServices
+{
+    public interface IProprietaryService
+    {
+        
+        List<Proprietary> ReadAll();
+        Proprietary GetById(int id);
+        Proprietary Create(string cadastreNumber, string forestryEnterprise, string City);
+        Proprietary Update(Proprietary proprietary);
+        Proprietary Delete(int id);
+    }
+}
