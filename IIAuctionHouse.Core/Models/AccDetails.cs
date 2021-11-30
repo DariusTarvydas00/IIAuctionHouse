bb5b963948f8d3e09d3017fe63c34e9940acd86f
DariusTarvydas00 <79032020+DariusTarvydas00@users.noreply.github.com>
2021-11-30 09:46:25 +0100

    AccDetails Model Test

---
+++ IIAuctionHouse.Core/Models/AccDetails.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace IIAuctionHouse.Core.Models
+{
+    public class AccDetails
+    {
+        public int Id { get; set; }
+        public Address Address { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public DateTime AccCreationDateTime { get; set; }
+    }
+}
