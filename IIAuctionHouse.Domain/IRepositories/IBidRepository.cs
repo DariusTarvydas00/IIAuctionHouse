56d84a9a74262ddae9fc7dd622c65c29eec92826
DariusTarvydas00 <79032020+DariusTarvydas00@users.noreply.github.com>
2021-11-30 23:03:14 +0100

    Bid Service Test

---
+++ IIAuctionHouse.Domain/IRepositories/IBidRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using IIAuctionHouse.Core.Models;
+
+namespace IIAuctionHouse.Domain.IRepositories
+{
+    public interface IBidRepository
+    {
+        public List<Bid> ReadAll();
+
+        public Bid GetById(int id);
+
+        public Bid Create(int bidAmount, DateTime bidDateTime);
+
+        public Bid Update(Bid bid);
+
+        public Bid Delete(int id);
+    }
+}
