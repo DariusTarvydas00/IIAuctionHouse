4d7f268c1c05c89dab2441affbcc58a5432a4c01
DariusTarvydas00 <79032020+DariusTarvydas00@users.noreply.github.com>
2021-11-30 22:32:43 +0100

    Bid Unit Test

---
+++ IIAuctionHouse.Core/Models/Bid.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace IIAuctionHouse.Core.Models
+{
+    public class Bid
+    {
+        public int Id { get; set; }
+        public int BidAmount { get; set; }
+        public DateTime BidDateTime { get; set; }
+    }
+}
