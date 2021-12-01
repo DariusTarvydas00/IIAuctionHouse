757a09e90510b0c1e1f8faab0d9ba78ba9ef6f0b
DariusTarvydas00 <79032020+DariusTarvydas00@users.noreply.github.com>
2021-12-01 13:02:17 +0100

    Interface Admin Service Test

---
+++ IIAuctionHouse.Core/IServices/IAdminService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using IIAuctionHouse.Core.Models;
+
+namespace IIAuctionHouse.Core.IServices
+{
+    public interface IAdminService
+    {
+        List<Admin> ReadAll();
+        Admin GetById(int id);
+        Admin Create(string firstName, string lastName, Address address, Proprietary proprietary, Bid bid);
+        Admin Update(Admin Admin);
+        Admin Delete(int id);
+    }
+}
