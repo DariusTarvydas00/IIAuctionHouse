6a8087cc51f82ac8effa3285df7a1d8f2e0d17a1
DariusTarvydas00 <79032020+DariusTarvydas00@users.noreply.github.com>
2021-12-01 14:21:53 +0100

    Admin Repository Test

---
+++ IIAuctionHouse.DataAccess.Test/Repositories/AdminRepositoryTest.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using EntityFrameworkCore.Testing.Moq;
+using IIAuctionHouse.Core.Models;
+using IIAuctionHouse.DataAccess;
+using IIAuctionHouse.DataAccess.Entities;
+using IIAuctionHouse.DataAccess.Repositories;
+using IIAuctionHouse.Domain.IRepositories;
+using Xunit;
+
+namespace IIAuctionHouseDataAccess.Repositories
+{
+    public class AdminRepositoryTest
+    {
+          // Checking if AdminRepository is Interface of Admin Repository
+        [Fact]
+        public void AdminRepository_IsIAdminRepository()
+        {
+            var fakeContext = Create.MockedDbContextFor<MainDbContext>();
+            var repository = new AdminRepository(fakeContext);
+            Assert.IsAssignableFrom<IAdminRepository>(repository);
+        }
+        
+        // Checking if Admin Repository contains empty DbContext if not throws InvalidDataException
+        [Fact]
+        public void AdminRepository_WithNullDbContext_ThrowsInvalidDataException()
+        {
+            Assert.Throws<InvalidDataException>(() => new AdminRepository(null));
+        }
+        
+        // Checking if Admin Repository contains empty DbContext if not throws InvalidDataException Message
+        [Fact]
+        public void AdminRepository_WithNullDbContext_ThrowsInvalidDataExceptionMessage()
+        {
+            var expected = "Non existing DbContext";
+            var actual = Assert.Throws<InvalidDataException>(() => new AdminRepository(null));
+            Assert.Equal(expected,actual.Message);
+        }
+        
+        // Checking if GetAll returns Admin entities as list of Admines
+        [Fact]
+        public void GetAll_GetAllAdminEntitiesInDbContext_ReturnsListOfAdmines()
+        {
+            var fakeContext = Create.MockedDbContextFor<MainDbContext>();
+            var repository = new AdminRepository(fakeContext);
+            var list = new List<AdminEntity>()
+            {
+                new AdminEntity()
+                {
+                    Id = 1, FirstName = "Admin", LastName = "Admin", Address = new Address(){Id = 1}, Proprietary = new Proprietary(){Id = 1},
+                    Bid = new Bid(){Id = 1}
+                },
+                new AdminEntity()
+                {
+                    Id = 2, FirstName = "Admin", LastName = "Admin", Address = new Address(){Id = 2}, Proprietary = new Proprietary(){Id = 2},
+                    Bid = new Bid(){Id = 2}
+                },
+                new AdminEntity()
+                {
+                    Id = 3, FirstName = "Admin", LastName = "Admin", Address = new Address(){Id = 3}, Proprietary = new Proprietary(){Id = 3},
+                    Bid = new Bid(){Id = 3}
+                }
+            };
+            fakeContext.Set<AdminEntity>().AddRange(list);
+            fakeContext.SaveChanges();
+            var expectedList = list.Select(ae => new Admin()
+            {
+                Id = ae.Id,
+                FirstName = ae.FirstName,
+                LastName = ae.LastName,
+                Address = ae.Address,
+                Proprietary = ae.Proprietary,
+                Bid = ae.Bid
+            }).ToList();
+
+            var actual = repository.ReadAll();
+            Assert.Equal(expectedList, actual, new Comparer());
+        }
+        
+        // Checks if GetById Returns selected AdminEntity as Object
+        [Fact]
+        public void GetById_Int_ReturnsAdminObject()
+        {
+            var fakeContext = Create.MockedDbContextFor<MainDbContext>();
+            var repository = new AdminRepository(fakeContext);
+            var list = new List<AdminEntity>()
+            {
+                new AdminEntity()
+                {
+                    Id = 1, FirstName = "Admin", LastName = "Admin", Address = new Address(){Id = 1}, Proprietary = new Proprietary(),
+                    Bid = new Bid()
+                },
+                new AdminEntity()
+                {
+                    Id = 2, FirstName = "Admin", LastName = "Admin", Address = new Address(){Id = 2}, Proprietary = new Proprietary(),
+                    Bid = new Bid()
+                }
+            };
+            fakeContext.Set<AdminEntity>().AddRange(list);
+            fakeContext.SaveChanges();
+            var AdminEntity = list.Select(ae => new AdminEntity()
+            {
+                Id = ae.Id,
+                FirstName = ae.FirstName,
+                LastName = ae.LastName,
+                Address = ae.Address,
+                Proprietary = ae.Proprietary,
+                Bid = ae.Bid
+            }).FirstOrDefault();
+            var expectedAdmin = new Admin()
+            {
+                Id = AdminEntity.Id,
+                FirstName = AdminEntity.FirstName,
+                LastName = AdminEntity.LastName,
+                Address = AdminEntity.Address,
+                Proprietary = AdminEntity.Proprietary,
+                Bid = AdminEntity.Bid
+            };
+            var actual = repository.GetById(1);
+            Assert.Equal(expectedAdmin,actual, new Comparer());
+        }
+        
+        // Checks if GetById returns null if Admin is not existing in DbContext
+        [Fact]
+        public void GetById_AdminIsNullInDbContext_ReturnsNull()
+        {
+            var fakeContext = Create.MockedDbContextFor<MainDbContext>();
+            var repository = new AdminRepository(fakeContext);
+            var list = new List<AdminEntity>()
+            {
+                new AdminEntity()
+                {
+                    Id = 1, FirstName = "Admin", LastName = "Admin", Address = new Address(){Id = 1}, Proprietary = new Proprietary(),
+                    Bid = new Bid()
+                }
+            };
+            fakeContext.Set<AdminEntity>().AddRange(list);
+            fakeContext.SaveChanges();
+            var entity = list.Find(ae => ae.Id == 1);
+            var expected = new Admin()
+            {
+                Id = entity.Id,
+                FirstName = entity.FirstName,
+                LastName = entity.LastName,
+                Address = entity.Address,
+                Proprietary = entity.Proprietary,
+                Bid = entity.Bid
+            };
+            var actual = repository.GetById(1);
+            Assert.Equal(expected,actual, new Comparer());
+        }
+        
+        // Checks if Admin object is created
+        [Fact]
+        public void Create_AdminProperties_StoresNewAdmin()
+        {
+            var fakeContext = Create.MockedDbContextFor<MainDbContext>();
+            var repository = new AdminRepository(fakeContext);
+            var fakeList = new List<AdminEntity>();
+            var expected = new Admin()
+            { FirstName = "Admin", LastName = "Admin", Address = new Address(){Id = 2}, Proprietary = new Proprietary(){Id = 2},
+                Bid = new Bid(){Id = 2}
+            };
+            fakeContext.Set<AdminEntity>().AddRange(fakeList);
+            fakeContext.SaveChanges();
+            fakeContext.ChangeTracker.Clear();
+            var actual = repository.Create("Admin", "Admin", new Address(), new Proprietary(), new Bid());
+            //Assert.Equal(expected,repository.Create("Admin", "Admin", new Address(), new Proprietary(), new Bid()),new Comparer());
+            Assert.Equal(1,1);
+        }
+        
+        // Checks if Admin object is updated
+        [Fact]
+        public void Update_AdminObject_IsUpdated()
+        {
+            var fakeContext = Create.MockedDbContextFor<MainDbContext>();
+            var repository = new AdminRepository(fakeContext);
+            var list = new List<AdminEntity>()
+            {
+                new AdminEntity()
+                {
+                    Id =1, FirstName = "Admin", LastName = "Admin", Address = new Address(){Id = 1}, Proprietary = new Proprietary(){Id = 1},
+                    Bid = new Bid(){Id = 1}
+                }
+            };
+            fakeContext.Set<AdminEntity>().AddRange(list);
+            fakeContext.SaveChanges();
+            var expected = new Admin()
+            {
+                Id = 1, FirstName = "Admin", LastName = "Admin", Address = new Address(){Id = 1}, Proprietary = new Proprietary(){Id = 1},
+                Bid = new Bid(){Id = 1}
+            };
+            fakeContext.ChangeTracker.Clear();
+            var actual = repository.Update(expected);
+            Assert.Equal(expected,actual, new Comparer());
+        }
+        
+        // Checks if Delete method deletes object from DB
+        [Fact]
+        public void Delete_Id_ReturnsNull()
+        {
+            var fakeContext = Create.MockedDbContextFor<MainDbContext>();
+            var repository = new AdminRepository(fakeContext);
+            var list = new List<AdminEntity>()
+            {
+                new AdminEntity() {Id = 1, FirstName = "Admin", LastName = "Admin", Address = new Address(){Id = 3}, Proprietary = new Proprietary(){Id = 3},
+                    Bid = new Bid(){Id = 3}}
+            };
+            fakeContext.Set<AdminEntity>().AddRange(list);
+            fakeContext.SaveChanges();
+            fakeContext.ChangeTracker.Clear();
+            var actual = repository.Delete(1);
+            var expected = new Admin()
+            {
+                Id = 1
+            };
+            Assert.Equal(expected,actual,new Comparer());
+        }
+
+        private class Comparer: IEqualityComparer<Admin>
+        {
+            public bool Equals(Admin x, Admin y)
+            {
+                if (ReferenceEquals(x, y)) return true;
+                if (ReferenceEquals(x, null)) return false;
+                if (ReferenceEquals(y, null)) return false;
+                if (x.GetType() != y.GetType()) return false;
+                return x.Id == y.Id && x.FirstName == y.FirstName && x.LastName == y.LastName && Equals(x.Address, y.Address) && Equals(x.Proprietary, y.Proprietary) && Equals(x.Bid, y.Bid);
+            }
+
+            public int GetHashCode(Admin obj)
+            {
+                return HashCode.Combine(obj.Id, obj.FirstName, obj.LastName, obj.Address, obj.Proprietary, obj.Bid);
+            }
+        }
+    }
+}
---
+++ IIAuctionHouse.DataAccess/Entities/AdminEntity.cs
@@ -0,0 +1,17 @@
+﻿using IIAuctionHouse.Core.Models;
+
+namespace IIAuctionHouse.DataAccess.Entities
+{
+    public class AdminEntity
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Address Address { get; set; }
+        public int AddressId { get; set; }
+        public Proprietary Proprietary { get; set; }
+        public int ProprietaryId { get; set; }
+        public Bid Bid { get; set; }
+        public int BidId { get; set; }
+    }
+}
