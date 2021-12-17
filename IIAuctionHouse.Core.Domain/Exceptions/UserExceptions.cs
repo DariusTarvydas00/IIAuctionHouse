1eb89b42be1d51bef0d73dd1258ece3c649d6d86
DariusTarvydas00 <79032020+DariusTarvydas00@users.noreply.github.com>
2021-12-17 01:02:50 +0100

    add

---
+++ IIAuctionHouse.Core.Domain/Exceptions/UserExceptions.cs
@@ -0,0 +1,8 @@
+﻿namespace IIAuctionHouse.Core.Domain.Exceptions
+{
+    public class UserExceptions
+    {
+        public const string UserRepoInvalid = "User Repository failure";
+        public const string UserIdInvalid = "User Id value is invalid";
+    }
+}
